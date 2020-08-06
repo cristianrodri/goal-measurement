c9b826f50865ac3fa1cae8befece2a6c4b1a3454
Cristian Rodriguez <contacto.cristianrodriguez@gmail.com>
2020-08-06 19:26:34 -0300

    change algorithms value in expressJwt

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ const expressJwt = require('express-jwt')
 // generate req.user in case of the token is valid
 const auth = expressJwt({
   secret: process.env.JWT_KEY,
-  algorithms: ['HS256']
+  algorithms: ['RS256']
 })
 
 module.exports = auth
