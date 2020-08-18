adbdb30edc7a07159cfdb3bafe4d3e5635444c9d
Cristian Rodriguez <contacto.cristianrodriguez@gmail.com>
2020-08-17 21:53:02 -0300

    Start creating end goal funcionality when date goal is reached

---
+++ client/src/components/goal-dashboard/ChangeEndDate.js
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react'
+import { makeStyles, Button } from '@material-ui/core'
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker
+} from '@material-ui/pickers'
+import DateFnsUtils from '@date-io/date-fns'
+import { GlobalContext } from '../../context/Context'
+import { SecondaryTitle } from '../Title'
+
+const useStyles = makeStyles(theme => ({
+  dateContainer: {
+    // display: 'flex',
+    // flexDirection: 'column',
+    // alignItems: 'center'
+  }
+}))
+const ChangeEndDate = () => {
+  const classes = useStyles()
+  const { state, dispatchEndDate } = useContext(GlobalContext)
+  return (
+    <div className={classes.dateContainer}>
+      <SecondaryTitle>Type a new date</SecondaryTitle>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          margin="normal"
+          id="date-picker-dialog"
+          label="Date picker dialog"
+          format="d MMMM yyyy"
+          value={state.endDate}
+          onChange={changedDate => dispatchEndDate(changedDate)}
+          KeyboardButtonProps={{
+            'aria-label': 'change date'
+          }}
+          disablePast
+        />
+      </MuiPickersUtilsProvider>
+      <Button>Change date</Button>
+    </div>
+  )
+}
+
+export default ChangeEndDate
