6b4f30ecc8937d47d2bb5ce164587599eeed737f
Cristian Rodriguez <contacto.cristianrodriguez@gmail.com>
2020-10-01 20:20:35 -0300

    use isWorkingDay from todayPerformance instead of selectedGoal activities

--- client/src/components/goal-dashboard/Performance.js
+++ client/src/components/goal-dashboard/Performance.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import { Paper } from '@material-ui/core'
-import moment from 'moment'
 import WorkingPerformance from './WorkingPerformance'
 import { useSelector } from 'react-redux'
 
 const Performance = () => {
-  const currentDay = moment().format('dddd').toLowerCase()
-  const state = useSelector(state => state.goal.selectedGoal)
-  const isWorkingDay = state.activities.some(
-    activity => activity.days[currentDay]
-  ) // Check if today is "Working day" by checking if one of the activities is true today
+  const isWorkingDay = useSelector(state => state.performance.todayPerformance)
 
   return (
     <div>
