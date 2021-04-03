760796a00721e0e4bd2344bf8e5610e6f91b3720
Cristian Rodriguez <contacto.cristianrodriguez@gmail.com>
2021-04-03 14:03:55 -0300

    ¿add correct types to functions

--- client/src/api/api_performance.ts
+++ client/src/api/api_performance.ts
@@ -1,15 +1,17 @@
-export const createNewDayPerformance = (goalId, data) => {
-  return fetch(`/api/${goalId}/createnewday`, {
-    method: 'PUT',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-    .then(res => res.json())
-    .catch(error => console.log(error))
-}
+// export const createNewDayPerformance = (goalId: string, data) => {
+//   return fetch(`/api/${goalId}/createnewday`, {
+//     method: 'PUT',
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+//   })
+//     .then(res => res.json())
+//     .catch(error => console.log(error))
+// }
+
+import { PerformanceActivity } from '../types'
 
 export const getAllPerformancesByGoal = (goalId: string, clientUTC: number) => {
   return fetch(`/api/${goalId}/performances/${clientUTC}`, {
@@ -23,7 +25,11 @@ export const getAllPerformancesByGoal = (goalId: string, clientUTC: number) => {
     .catch(err => console.log(err))
 }
 
-export const updatePerformanceDay = (goalId, performanceId, data) => {
+export const updatePerformanceDay = (
+  goalId: string,
+  performanceId: string,
+  data: { done: boolean; activities: PerformanceActivity[] }
+) => {
   return fetch(`/api/${goalId}/updatePerformanceDay/${performanceId}`, {
     method: 'PUT',
     headers: {
