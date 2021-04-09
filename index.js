bac127cab14dc0420312c4a7bf8c340c4baf1f5d
Cristian Rodriguez <contacto.cristianrodriguez@gmail.com>
2021-04-09 15:07:12 -0300

    change log color

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ const port = process.env.PORT || 3001
 server.use(app)
 
 server.listen(port, () =>
-  console.log(`Server running on port ${port}`.yellow.bold)
+  console.log(`Server running on port ${port}`.green.bold)
 )
